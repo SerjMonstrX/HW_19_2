b305eddf7c13cb17c0cc925002ab5b992224c222
SerjMonstrX <SerjMonstrX@gmail.com>
2024-01-14 23:13:16 +0700

    20.1.2-3 Созданы модели Category, Product, описаны поля.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', blank=True, null=True)
+    image = models.ImageField(upload_to='product_image/', verbose_name='изображение', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за штуку')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    last_change_date = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('price',)
+
+
