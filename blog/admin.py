61bf1faea77f13b8d633326d46b68b04f72146eb
SerjMonstrX <SerjMonstrX@gmail.com>
2024-01-22 04:21:32 +0700

    20.1.2-3 Готово

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'creation_date', 'is_published', 'view_count')
+    list_filter = ('is_published',)
+    search_fields = ('title',)
--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView, BlogListView
+from blog.views import (
+    BlogCreateView,
+    BlogListView,
+    BlogDetailView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create'),
+    path('<slug:slug>/', BlogDetailView.as_view(), name='detail'),
+    path('<slug:slug>/edit/', BlogUpdateView.as_view(), name='edit'),
+    path('<slug:slug>/delete/', BlogDeleteView.as_view(), name='delete'),
     path('', BlogListView.as_view(), name='list'),
-    # path('view/<int:pk>/', ..., name='edit'),
-    # path('edit/<int:pk>/', ..., name='edit'),
-    # path('delete/<int:pk>/', ..., name='delete'),
 ]
